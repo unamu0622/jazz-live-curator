6daabd814c7ee416648bb323e48bb28998845239
hiroshi.ito.0622@gmail.com <hiroshi.ito.0622@gmail.com>
2019-03-13 17:57:10 +0900

    Add curator models

--- jazzlivecurator/curator/admin.py
+++ jazzlivecurator/curator/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Live
 
-# Register your models here.
+
+admin.site.register(Live)
--- jazzlivecurator/curator/models.py
+++ jazzlivecurator/curator/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Club(models.Model):
+    club_name = models.CharField('店', max_length=200)
+    url = models.CharField('URL', max_length=200)
+    address = models.CharField('住所', max_length=200)
+    phone_number = models.CharField('電話番号', max_length=200)
+    email = models.EmailField('Email')
+
+    def __str__(self):
+        return self.club_name
+
+
+class Player(models.Model):
+    player_name = models.CharField('プレイヤー', max_length=200)
+    instrument = models.CharField('楽器', max_length=200)
+
+    def __str__(self):
+        return self.player_name
+
+
+class Live(models.Model):
+    live_name = models.CharField('ライブ', max_length=200)
+    club = models.ForeignKey(Club, on_delete=models.PROTECT)
+    player = models.ForeignKey(Player, on_delete=models.PROTECT)
+    open_time = models.DateTimeField('オープン')
+    start_time = models.DateTimeField('スタート')
+
+    def __str__(self):
+        return self.live_name
